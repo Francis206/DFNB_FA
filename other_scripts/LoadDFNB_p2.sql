a56842e59e1846722a7bbaa2efe6c1e9c887b547
Francis <francisr.a@hotmail.com>
2020-12-06 01:46:49 -0700

    Added enhancements for LDSBC IT 240 - Project 2.4: DFNB - Evaluate Current Performance - Create v1 of Script Repository

---
+++ other_scripts/LoadDFNB_p2.sql
@@ -0,0 +1,60 @@
+/*****************************************************************************************************************
+NAME:   LoadDFNB_p2
+PURPOSE: Project 2
+
+SUPPORT: 
+
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       12/3/2019   FAgballog      1. Built tables for project 2 for all related tables 
+
+
+RUNTIME: 
+Approx. 1 min
+
+NOTES:
+
+
+
+LICENSE: This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+ 
+******************************************************************************************************************/
+-- 2.1 loand into table dbo.tblTranTypeDim
+
+USE DFNB2;
+
+TRUNCATE TABLE dbo.tblTranTypeDim;
+
+INSERT INTO dbo.tblTranTypeDim
+SELECT DISTINCT 
+       tran_type_id
+     , tran_type_code
+     , tran_type_desc
+     , tran_fee_prct
+	 , cur_cust_req_ind
+  FROM dbo.stg_p2 AS stg
+ ORDER BY  1 ;
+
+-- 2.2  loand into table dbo.tblTranFact
+
+
+USE DFNB2;
+
+TRUNCATE TABLE dbo.tblTranFact;
+
+INSERT INTO dbo.tblTranFact
+SELECT DISTINCT 
+       [branch_id]
+      ,[acct_id]
+      ,[tran_date]
+      ,[tran_time]
+      ,[tran_type_id]
+      ,[tran_amt]
+      ,[tran_fee_amt]
+  FROM [DFNB2].[dbo].[stg_p2]
+  ORDER BY [branch_id]
